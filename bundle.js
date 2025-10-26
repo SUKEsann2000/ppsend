19f944d8ad682e56c418f7760a4e86437355d24a
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2025-10-26 21:28:57 +0900

    Add env in bundle.js

--- bundle.js
+++ bundle.js
@@ -7,5 +7,9 @@ esbuild.build({
   target: ['node22'],       // Node v22 向け
   outfile: 'dist/bundle.js', 
   sourcemap: false,
-  minify: false
+  minify: false,
+  define: {
+    'process.env.WS_URL': JSON.stringify(process.env.WS_URL || ''),
+    'process.env.DISCORD_WEBHOOK': JSON.stringify(process.env.DISCORD_WEBHOOK || ''),
+  },
 }).catch(() => process.exit(1));
