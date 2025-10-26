3b5582b94e7bdaf31bfd175ee2e1343b5f015e98
SUKEsann2000 <129367282+SUKEsann2000@users.noreply.github.com>
2025-10-26 22:15:38 +0900

    use default when WS_URL isn’t started with ws://

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (fs.existsSync(envPath)) {
 }
 
 console.log("WS_URL: ", process.env.WS_URL);
-if (!process.env.WS_URL || process.env.WS_URL.length === 0) {
+if (!process.env.WS_URL || !process.env.WS_URL.startsWith("ws://")) {
   process.env.WS_URL = "ws://127.0.0.1:24050/websocket/v2";
 }
 
