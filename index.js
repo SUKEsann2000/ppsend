d5958810967ff653bf7d2661204a8b898d728915
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2025-10-26 22:03:17 +0900

    Remove .env file creation step and set process.env.WS_URL if it is invailed

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ if (fs.existsSync(envPath)) {
   });
 }
 
+if (!process.env.WS_URL || ) {
+  process.env.WS_URL = "ws://127.0.0.1:24050/websocket/v2";
+}
+
 // Counter for WebSocket connection attempts
 let wscount = 0;
 
