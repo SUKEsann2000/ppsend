99a68ac80749143ea70fabc5068700fe39363ade
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2025-10-31 21:43:28 +0900

    rename bundle.js to bundler.js or bundled.js

--- bundler.js
+++ bundler.js
@@ -5,7 +5,7 @@ esbuild.build({
   bundle: true,             // 依存を全部まとめる
   platform: 'node',         // Node.js 用
   target: ['node22'],       // Node v22 向け
-  outfile: 'dist/bundle.js', 
+  outfile: 'dist/bundled.js', 
   sourcemap: false,
   minify: false,
   format: 'cjs',
